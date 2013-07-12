be288a26f1ad4e093f00a62403eca211d9ed9700
clowwindy <clowwindy42@gmail.com>
2013-07-12 14:53:28 +0800

    gitignore

---
+++ LevelDB/leveldb.h
@@ -0,0 +1,13 @@
+//
+//  leveldb.h
+//  leveldb
+//
+//  Created by clowwindy on 6/26/13.
+//  Copyright (c) 2013 Pave Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface leveldb : NSObject
+
+@end
---
+++ LevelDB/leveldb.m
@@ -0,0 +1,13 @@
+//
+//  leveldb.m
+//  leveldb
+//
+//  Created by clowwindy on 6/26/13.
+//  Copyright (c) 2013 Pave Labs. All rights reserved.
+//
+
+#import "leveldb.h"
+
+@implementation leveldb
+
+@end
